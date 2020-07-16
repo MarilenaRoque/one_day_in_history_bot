9f765bd7402a7e04b7eeefb1928c28d2a3d7e52a
MarilenaRoque <marilenas@id.uff.br>
2020-07-15 23:07:52 -0300

    Building the tweet content
    Build Post Class
    Build method to randonly choose an event
    Build method to form the string with the tweet content

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,7 @@
 require './lib/bot.rb'
 require 'twitter'
 require './lib/json_data.rb'
+require './lib/tweet.rb'
 
 
 bot = Twitter_bot.new
@@ -10,5 +11,11 @@ bot = Twitter_bot.new
 1.times do
   time = Time.new
   data_string_daily = JsonData.new(time.day, time.month)
-  bot.client.update(" On this same day in the year of #{data_string_daily.json_hash["events"][3]["year"]} it happened: #{data_string_daily.json_hash["events"][3]["description"]}")
+  daily_tweet = Post.new(data_string_daily.json_hash)
+  # bot.client.update("On this same day in the year of #{data_string_daily.json_hash["events"][3]["year"]} it happened: #{data_string_daily.json_hash["events"][3]["description"]}")
+  content = false
+  until content != false && content.length < 280 do
+    puts content = daily_tweet.tweet_content
+  end
 end
+# puts daily_post.event_post_string
